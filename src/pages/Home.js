b267b6c86e4203abffe3bff0f01236d5f2e95eeb
Salih Alperen <sal.alperen1@gmail.com>
2022-08-17 23:12:19 +0200

    Added all parts of homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,6 +15,16 @@ const Home = () => {
         recentLaunch='recent launch'
         buttonText='rewatch'
       />
+      <SectionWithButton
+        img='https://www.spacex.com/static/images/backgrounds-2022/starship-update/S20_Chopstick_Stack_Desktop.webp'
+        text='starship update'
+        buttonText='learn more'
+      />
+      <SectionWithButton
+        img='https://www.spacex.com/static/images/backgrounds-2021/hls-resized-2.webp'
+        text='Starship to Land NASA Astronauts on the Moon'
+        buttonText='learn more'
+      />
     </>
   );
 };
