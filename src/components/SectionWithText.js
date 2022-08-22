3c82d89825da23bdafec31a264872b1110d9c801
Salih Alperen <sal.alperen1@gmail.com>
2022-08-22 22:43:00 +0200

    Added text based layout

--- src/components/SectionWithText.js
+++ src/components/SectionWithText.js
@@ -1,7 +1,31 @@
 import React from 'react';
-
-const SectionWithText = () => {
-  return <div>SectionWithText</div>;
+import styled from 'styled-components';
+const SectionWithText = ({ imgUrl, text, exp }) => {
+  return (
+    <Wrapper style={{ background: `url(${imgUrl})` }}>
+      <div className='banner'>
+        <h1>{text}</h1>
+        <p>{exp}</p>
+      </div>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.section`
+  height: 100vh;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  display: grid;
+  place-items: center;
+  h1 {
+    text-transform: uppercase;
+    font-size: 5rem;
+  }
+  p {
+    text-align: center;
+    text-transform: uppercase;
+  }
+`;
 
 export default SectionWithText;
--- src/pages/FalconHeavy.js
+++ src/pages/FalconHeavy.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import SectionWithText from '../components/SectionWithText';
 
 const FalconHeavy = () => {
-  return <div>FalconHeavy</div>;
+  return (
+    <div>
+      <SectionWithText
+        imgUrl={require('../assets/falcon-heavy.webp')}
+        text='falcon heavy'
+        exp={"the world's most powerful rocket"}
+      />
+    </div>
+  );
 };
 
 export default FalconHeavy;
